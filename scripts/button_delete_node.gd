951eba9c739ffdb2254bf473456768ce190bca11
Mehdi <mhdmsy@hotmail.co.uk>
2019-09-08 00:44:15 +0100

    updates

--- scripts/button_delete_node.gd
+++
@@ -1,9 +0,0 @@
-#Copyright (c) 2019 Mehdi Msayib#
-extends Button
-
-
-
-func _on_button_delete_node_toggled(button_pressed):
-	global_var.delete_node=button_pressed
-	pass # replace with function body
-
--- scripts/fps.gd
+++
@@ -1,9 +0,0 @@
-#Copyright (c) 2019 Mehdi Msayib#
-extends Label
-
-
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	text="FPS : " + str(Engine.get_frames_per_second())
